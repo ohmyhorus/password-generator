1eef9cde7379d14c391b999a51a372fb0a8c45aa
Sasha <45601549+ohmyhorus@users.noreply.github.com>
2023-09-05 15:44:07 +0200

    Update password gen.py

--- password gen.py
+++ password gen.py
@@ -14,3 +14,13 @@ def gen():
     
     s = []
     s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
+    
+    
+    random.shuffle(s)
+    pas = ("".join(s[0:passlen]))
+    print(pas)
+    
+gen()
