bb680930360682af29d9e015978fefcb5060c5b4
Sasha <45601549+ohmyhorus@users.noreply.github.com>
2023-10-22 13:21:37 +0200

    Update password gen.py

--- password gen.py
+++ password gen.py
@@ -1,26 +1,11 @@
-import string
 import random
+import string
 
-def gen():
-    s1 = string.ascii_uppercase
-    
-    s2 = string.ascii_lowercase
-    
-    s3 = string.digits
-    
-    s4 = string.punctuation
-    
-    passlen = int(input("Enter the password length\n"))
-    
-    s = []
-    s.extend(list(s1))
-    s.extend(list(s2))
-    s.extend(list(s3))
-    s.extend(list(s4))
-    
-    
-    random.shuffle(s)
-    pas = ("".join(s[0:passlen]))
-    print(pas)
-    
-gen()
+
+def generate_password(min_lenght, numbers=True, special_characters=True):
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+
+
+generate_password(10)
