9dbec7a25074356126313bd53cefbdc60dafe7f6
Sasha <45601549+ohmyhorus@users.noreply.github.com>
2023-10-24 21:14:01 +0200

    Update password gen.py

--- password gen.py
+++ password gen.py
@@ -6,15 +6,26 @@ def generate_password(min_lenght, numbers=True, special_characters=True):
     letters = string.ascii_letters
     digits = string.digits
     special = string.punctuation
-    
+
     characters = letters
     if numbers:
         characters += digits
     if special_characters:
         characters += special
-        
+
     pwd = ""
     meets_criteria = False
+    has_number = False
+    has_special = False
+
+    while not meets_criteria or len(pwd) < min_lenght:
+        new_char = random.choice(characters)
+        pwd += new_char
+
+        if new_char in digits:
+            has_number = True
+        elif new_char in special:
+            has_special = True
 
 
 generate_password(10)
